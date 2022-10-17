7e4fc3f7736e8ab8f80c6978057f84eed6648736
Ian Skelskey <ianskelskey@gmail.com>
2022-10-17 18:46:13 -0400

    Clean out repo

--- js/app.js
+++
@@ -1,3 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/minimal-pwa/serviceworker.js");
-}
--- serviceworker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", event => {
-    console.log("Service worker installed", event);
-});
-
-self.addEventListener("fetch", event => {
-    console.log("Service worker fetching...", event)
-})
