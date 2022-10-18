e038723a413c89486fd6c01b26885ddf83da173d
Ian Skelskey <ianskelskey@gmail.com>
2022-10-17 22:13:08 -0400

    Rename prettier workflow

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import * as serviceWorker from './serviceWorker';
 import firebase from './util/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
+
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
 serviceWorker.register();
