1559655f4fc31fd8267f818b778fe57187869c9f
Ian Skelskey <ianskelskey@gmail.com>
2022-10-20 02:47:44 -0400

    Add win condition

--- src/tictactoe/Board.js
+++ src/tictactoe/Board.js
@@ -3,7 +3,8 @@ import Square from './Square';
 
 function Board() {
     const [state, setState] = useState({
-        squares: Array(9).fill('\n'),
+        status: "Welcome to Tic-Tac-Toe. X goes first!",
+        squares: Array(9).fill(null),
         xIsNext: true,
     });
 
@@ -12,15 +13,27 @@ function Board() {
         for (let i = 0; i < 9; i++) {
             board[i] = renderSquare(i);
         }
+
+        const winner = calculateWinner(state.squares);
+        if (winner) {
+            state.status = 'Winner: ' + winner;
+        } else {
+            console.log('Next player: ' + (state.xIsNext ? 'X' : 'O'));
+            state.status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+        }
+
         return board;
     }
 
     function renderSquare(i) {
-        return <Square value={state.squares[i]} onClick={() => handleClick(i)} />;
+        return <Square value={state.squares[i]} key={i} onClick={() => handleClick(i)} />;
     }
 
     function handleClick(i) {
         const squares = state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = state.xIsNext ? 'X' : 'O';
         setState({
             squares: squares,
@@ -28,11 +41,31 @@ function Board() {
         });
     }
 
-    // const squares =
-    const status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                if(squares[a] !== null && squares[b] !== null && squares[c] !== null) {
+                    return squares[a];
+                }
+            }
+        }
+        return null;
+    }
+
     return (
         <div>
-            <h2 className="status">{status}</h2>
+            <h2 className="status">{state.status}</h2>
             <div className="board">{renderBoard()}</div>
         </div>
     );
