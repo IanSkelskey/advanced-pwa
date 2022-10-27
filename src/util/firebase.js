649565cd045028e1b2bc765742f5f606d3220065
Ian Skelskey <ianskelskey@gmail.com>
2022-10-26 22:08:40 -0400

    Test firestore

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
+import { getFirestore, collection, addDoc } from 'firebase/firestore/'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +19,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 // eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
+
+try {
+    const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    });
+    console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+    console.error("Error adding document: ", e);
+}
