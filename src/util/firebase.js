5cb50c52741be75668de414de570a4a635a2239c
Ian Skelskey <ianskelskey@gmail.com>
2022-10-26 22:29:02 -0400

    Firestore test 3

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, setDoc, getDocs, collection } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -33,3 +33,8 @@ const docData = {
 };
 setDoc(testDoc, docData);
 console.log('Wrote to Firestore');
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+});
